658669de8d0d800ce5ef8a3e8dbf40dbc9a1c915
Jey <ash_88kj@yahoo.com>
2021-03-31 23:00:05 +0800

    feat(update-qty)

--- src/view/product/details.js
+++ src/view/product/details.js
@@ -5,6 +5,7 @@ import { usersService } from "../../_services";
 const Details = (props) => {
   const { history } = props;
   const [usersState, setUsersState] = useState(null);
+  const [qty, setQty] = useState(0);
   const { id } = useParams();
   const [formState, setFormState] = useState({ values: {}, errors: [] });
   useEffect(() => {
@@ -133,11 +134,18 @@ const Details = (props) => {
           <div class="flex">
             <span class="mr-3">Quantity</span>
             <div class="flex flex-row h-10 w-full rounded-lg relative bg-transparent mt-1">
-                <button data-action="decrement" class=" bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-l cursor-pointer outline-none">
+                <button data-action="decrement" class=" bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-l cursor-pointer outline-none"
+                onClick= {() => setQty(qty - 1)}
+                >
                 <span class="m-auto text-2xl font-thin">−</span>
                 </button>
-                <input type="number" class="outline-none focus:outline-none text-center w-full bg-gray-300 font-semibold text-md hover:text-black focus:text-black  md:text-basecursor-default flex items-center text-gray-700  outline-none" name="custom-input-number" value="0"></input>
-            <button data-action="increment" class="bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-r cursor-pointer">
+                <input type="number" class="outline-none focus:outline-none text-center w-full bg-gray-300 font-semibold text-md hover:text-black focus:text-black  md:text-basecursor-default flex items-center text-gray-700  outline-none" 
+                  name="custom-input-number" 
+                  value={ qty }
+                ></input>
+            <button data-action="increment" class="bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-r cursor-pointer"
+            onClick= {() => setQty(qty + 1)}
+            >
                 <span class="m-auto text-2xl font-thin">+</span>
             </button>
             </div>
